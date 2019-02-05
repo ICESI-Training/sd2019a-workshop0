671c363278c95d727d778968d107892a1843696e
StevenMontealegre <james.montealegre@correo.icesi.edu.co>
2019-02-04 21:15:38 -0500

    add file

---
+++ install_web.rb
@@ -0,0 +1,16 @@
+yum_package 'nmap' do
+  action :install
+end
+
+yum_package 'httpd' do
+  action :install
+end
+
+remote_file '/var/www/html/index.html' do
+  source 'http://www.icesi.edu.co/'
+  action :create
+end
+
+service 'httpd' do
+ action :start
+end
